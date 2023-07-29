7b1100b444395c9e6432ed79d0fb236472f16c47
Imron Mirzarasulov <imronimronov32@gmail.com>
2023-07-29 22:31:43 +0500

    add auth with clerk

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Dashboard() {
-  return <Button variant={"destructive"}>Click me</Button>;
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function Dashboard() {
   return <Button variant={"destructive"}>Click me</Button>;
 }
