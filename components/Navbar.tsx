c7318f185ad6fdb30ee8f2626727a531a42fb853
Imron Mirzarasulov <imronimronov32@gmail.com>
2023-07-30 16:49:57 +0500

    add conversation page & heading component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
-import MobileSidebar from "@/components/mobile-sidebar";
+import MobileSidebar from "@/components/MobileSidebar";
 
 const Navbar = () => {
   return (
