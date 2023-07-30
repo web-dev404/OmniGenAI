9c7bdd91ab56ba981772ac3f42a7b0e143aee072
Imron Mirzarasulov <imronimronov32@gmail.com>
2023-07-30 16:24:33 +0500

    add more tools

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { ArrowRight, MessageSquare } from "lucide-react";
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+} from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 
@@ -10,7 +17,35 @@ const tools = [
     href: "/conversation",
     icon: MessageSquare,
     color: "text-violet-500",
-    bgColor: "bg-violet-50",
+    bgColor: "bg-violet-500/10",
+  },
+  {
+    label: "Music Generation",
+    href: "/music",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+  },
+  {
+    label: "Image Generation",
+    href: "/image",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+  },
+  {
+    label: "Video Generation",
+    href: "/video",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+  },
+  {
+    label: "Code Generation",
+    href: "/code",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
   },
 ];
 
